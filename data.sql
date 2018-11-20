7ff9fc0918a24e20f9111a64a4bd6a89b5507753
hafsa-fatima <hafsa.fatima995@gmail.com>
2018-11-19 19:21:06 -0600

    data by hafsa

--- data.sql
+++ data.sql
@@ -1,17 +1,17 @@
-insert into user_role (Name, description) values ('admini','Administrator');
-insert into user_role (Name, description) values  ('teacher','Staff');
-insert into user_role (Name, description) values  ('student',NULL);
+insert into user_role (Name, description) values ('Administration','Administrator');
+insert into user_role (Name, description) values  ('Teachers','Staff');
+insert into user_role (Name, description) values  ('Students',NULL);
 
-insert into user_account (Name,phone_no,role) values ('hafsa','682-553-6098',101);
-insert into user_account (Name,phone_no,role) values ('asmita','282-553-6098',102);
-insert into user_account (Name,phone_no,role) values ('huda','382-553-6098',103);
-insert into user_account (Name,phone_no,role) values ('rachael','882-553-6098',101);
-insert into user_account (Name,phone_no,role) values ('vamsi','982-953-6098',102);
-insert into user_account (Name,phone_no,role) values ('alam','682-553-6098',103);
-insert into user_account (Name,phone_no,role) values ('maryam','642-553-6098',101);
-insert into user_account (Name,phone_no,role) values ('linda','680-553-6028',102);
-insert into user_account (Name,phone_no,role) values ('rose','682-553-6238',103);
-insert into user_account (Name,phone_no,role) values ('fin','682-553-5348',101);
+insert into user_account (Name,phone_no,role) values ('F. Hafsa','682-553-6098',101);
+insert into user_account (Name,phone_no,role) values ('O. Asmita','282-553-6098',102);
+insert into user_account (Name,phone_no,role) values ('F. Huda','382-553-6098',103);
+insert into user_account (Name,phone_no,role) values ('S. Rachael','882-553-6098',101);
+insert into user_account (Name,phone_no,role) values ('T. Velma','982-953-6098',102);
+insert into user_account (Name,phone_no,role) values ('S. Aalam','682-553-6098',103);
+insert into user_account (Name,phone_no,role) values ('M. Maryam','642-553-6098',101);
+insert into user_account (Name,phone_no,role) values ('B. Lynda','680-553-6028',102);
+insert into user_account (Name,phone_no,role) values ('M. Rose','682-553-6238',103);
+insert into user_account (Name,phone_no,role) values ('P. Fin','682-553-5348',101);
 
 insert into p_Privileges (privilege_col,privilege_type) values ('Create_table','Account');
 insert into p_Privileges (privilege_col,privilege_type) values ('Drop','Account');
@@ -21,13 +21,13 @@ insert into p_Privileges (privilege_col,privilege_type) values ('Update','Accoun
 insert into p_Privileges (privilege_col,privilege_type) values ('Select','Relation');
 insert into p_Privileges (privilege_col,privilege_type) values ('Create_view','Relation');
 
-insert into tables values ('assignmentSubmits',1002);
-insert into tables values ('available_course',1001);
-insert into tables values ('degreeMap',1001);
-insert into tables values ('marks',1008);
-insert into tables values ('prerequisite',1008);
-insert into tables values ('projects',1007);
-insert into tables values ('registered_course',1005);
-insert into tables values ('salary',1004);
-insert into tables values ('semester_course',1002);
-insert into tables values ('teaching_courses',1001);
+insert into tables values ('Assignment_Submits',1002);
+insert into tables values ('Available_Course',1001);
+insert into tables values ('DegreeMap',1001);
+insert into tables values ('Marks',1008);
+insert into tables values ('Prerequisite',1008);
+insert into tables values ('Projects',1007);
+insert into tables values ('Registered_course',1005);
+insert into tables values ('Salary',1004);
+insert into tables values ('Semester_course',1002);
+insert into tables values ('Teaching_courses',1001);
